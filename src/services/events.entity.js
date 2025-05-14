4273c23f601dbe98f316ab56777c255824a8c81c
Piero Elescano <pieroelescano300@gmail.com>
2025-05-13 21:15:09 -0500

    First commit

---
+++ src/services/events.entity.js
@@ -0,0 +1,9 @@
+export interface Event {
+    "id": "1",
+    "name": "Optimized full-range structure",
+    "date": "2025-10-25",
+    "description": "View grow future call investment summer politics low. Lot policy police once director court six. Do Congress keep space then benefit of.",
+    "ticket_id": "66",
+    "organiser_id": "98",
+    "place_id": "75"
+  },}
